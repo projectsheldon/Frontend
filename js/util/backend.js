7aeea876de39d87ab5804e2a747454b601043c11
OgPenguu <penguusoftware@gmail.com>
2026-05-09 13:12:28 +0300

    Update backend.js

--- js/util/backend.js
+++ js/util/backend.js
@@ -3,7 +3,7 @@ const Api = {
 
     async _fetchBackendUrl()
     {
-        const remoteServer = 'http://e0wcwoocg4w0w0gc404wgswg.82.153.202.158.sslip.io';
+        const remoteServer = 'https://sheldon-backend.amaskeddev.workers.dev/';
         const localServer = 'http://localhost:3350';
 
         if(this._backendUrl) return this._backendUrl;
